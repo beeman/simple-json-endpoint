179d54a5acac5f832649667344e3d6a7b18a3aae
Bram Borggreve <beeman@beeman.nl>
2024-02-23 17:07:11 +0000

    feat: enable CORS

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import {Hono} from 'hono'
+import {cors} from 'hono/cors'
 import {wallets} from "./source";
 
 const app = new Hono()
+app.use(cors())
 
 app.get('/', (c) => {
   return c.json(wallets)
